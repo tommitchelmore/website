a0a123511f48c6d03346150cb5c2cd48eaae7e21
Thomas Mitchelmore <tommitchelmore@outlook.com>
2022-09-08 00:02:56 +0100

    feat: start setting up storyblok and eslint

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,55 +1,72 @@
-import type { JSX, ParentProps } from "solid-js";
-import { Dynamic } from "solid-js/web"
+/* eslint-disable sonarjs/no-duplicate-string */
+import { JSX, mergeProps, ParentProps } from 'solid-js'
+import { Dynamic } from 'solid-js/web'
 
-function Typography({
-    variant = "body",
-    ...props
-}: TypographyProps) {
+function Typography(_props: TypographyProps) {
+  const props = mergeProps({ variant: 'body' }, _props)
 
-    const tag = (props.tag || (props.emphasis && "em") || {
-        'display': 'h1',
-        'heading': 'h2',
-        'title': 'h3',
-        'body': 'p',
-        'caption': 'span',
-    }[variant]) as keyof JSX.IntrinsicElements;
+  const tag = () =>
+    (props.tag ||
+      (props.emphasis && 'em') ||
+      {
+        display: 'h1',
+        heading: 'h2',
+        title: 'h3',
+        body: 'p',
+        caption: 'span',
+      }[props.variant]) as keyof JSX.IntrinsicElements
 
-    const sizeClass = {
-        'display': 'text-display-small md:text-display-medium xl:text-display-large',
-        'heading': 'text-heading-small md:text-heading-medium xl:text-heading-large',
-        'title': 'text-title-small md:text-title-medium xl:text-title-large',
-        'body': 'text-body-small md:text-body-medium xl:text-body-large',
-        'caption': 'text-caption-small md:text-caption-medium xl:text-caption-large',
-    }[variant]
+  const sizeClass = () =>
+    ({
+      display:
+        'text-display-small md:text-display-medium xl:text-display-large',
+      heading:
+        'text-heading-small md:text-heading-medium xl:text-heading-large',
+      title: 'text-title-small md:text-title-medium xl:text-title-large',
+      body: 'text-body-small md:text-body-medium xl:text-body-large',
+      caption:
+        'text-caption-small md:text-caption-medium xl:text-caption-large',
+    }[props.variant])
 
-    const fontClass = props.emphasis ? 'font-medium !font-serif not-italic' : {
-        'display': 'font-serif font-medium',
-        'heading': 'font-serif font-medium',
-        'title': 'font-serif font-medium',
-        'body': 'font-sans',
-        'caption': 'font-sans'
-    }[variant]
+  const fontClass = () =>
+    props.emphasis
+      ? 'font-medium !font-serif not-italic'
+      : {
+          display: 'font-serif font-medium',
+          heading: 'font-serif font-medium',
+          title: 'font-serif font-medium',
+          body: 'font-sans',
+          caption: 'font-sans',
+        }[props.variant]
 
-    const colorClass = props.emphasis ? 'text-amaranth dark:text-amaranth' : {
-        'display': 'text-vulcan dark:text-haze',
-        'heading': 'text-vulcan dark:text-haze',
-        'title': 'text-vulcan dark:text-haze',
-        'body': 'text-vulcan/80 dark:text-haze/80',
-        'caption': 'text-vulcan/80 dark:text-haze/80',
-    }[variant]
+  const colorClass = () =>
+    props.emphasis
+      ? 'text-amaranth dark:text-amaranth'
+      : {
+          display: 'text-vulcan dark:text-haze',
+          heading: 'text-vulcan dark:text-haze',
+          title: 'text-vulcan dark:text-haze',
+          body: 'text-vulcan/80 dark:text-haze/80',
+          caption: 'text-vulcan/80 dark:text-haze/80',
+        }[props.variant]
 
-    return (
-        <Dynamic component={tag} class={`${sizeClass} ${fontClass} ${colorClass} ${props.class ?? ''}`}>
-            {props.children}
-        </Dynamic>
-    );
+  return (
+    <Dynamic
+      component={tag()}
+      class={`${sizeClass()} ${fontClass()} ${colorClass()} ${
+        props.class ?? ''
+      }`}
+    >
+      {props.children}
+    </Dynamic>
+  )
 }
 
 type TypographyProps = ParentProps & {
-    class?: string,
-    variant?: 'display' | 'heading' | 'title' | 'body' | 'caption';
-    emphasis?: boolean;
-    tag?: keyof JSX.IntrinsicElements;
+  class?: string
+  variant?: 'display' | 'heading' | 'title' | 'body' | 'caption'
+  emphasis?: boolean
+  tag?: keyof JSX.IntrinsicElements
 }
 
-export default Typography;
+export default Typography
